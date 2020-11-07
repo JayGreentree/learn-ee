ce012240ab587dc1b7c845694af7be3b18361557
Doug Black Jr <dougblackjr@gmail.com>
2020-11-07 17:00:30 -0500

    Step 1: Installation and Set Up

---
+++ README.md
@@ -0,0 +1,19 @@
+# Learn EE
+
+This is a sample project utilizing ExpressionEngine, so you can follow along and build an EE site from scratch.
+
+## Install
+
+It is highly recommended that you watch the videos at [ExpressionEngine University](https://u.expressionengine.com/) to walk through the full installation process. This repo should serve as a guide to follow along.
+
+If you want to install this repo:
+1. `git clone git@github.com:dougblackjr/learn-ee.git`
+2. Import the database from the database folder.
+
+## Usage
+
+Each branch in this repo will serve as a separate step in the video, so you are able to checkout any branch, and see the site that exists at the end of each video.
+
+The `main` branch will serve as the most up to date version of the site
+
+### Branches:
---
+++ system/user/config/config.php
@@ -0,0 +1,25 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// ExpressionEngine Config Items
+// Find more configs and overrides at
+// https://docs.expressionengine.com/latest/general/system_configuration_overrides.html
+
+$config['app_version'] = '6.0.0-b.3';
+$config['encryption_key'] = '7454e2a0984c5f73cb2ae0c9c34ad13f1c24ab4c';
+$config['session_crypt_key'] = '1c38c71f77029bcb41d83d2a0747bda9d377f6cd';
+$config['database'] = array(
+	'expressionengine' => array(
+		'hostname' => 'localhost',
+		'database' => 'learn_ee',
+		'username' => 'root',
+		'password' => '',
+		'dbprefix' => 'exp_',
+		'char_set' => 'utf8mb4',
+		'dbcollat' => 'utf8mb4_unicode_ci',
+		'port'     => ''
+	),
+);
+$config['show_ee_news'] = 'y';
+$config['share_analytics'] = 'y';
+
+// EOF
